588abb62db8b0130f298c7fe77af84bcb8001a6c
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-10-03 22:00:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,11 @@ def get_japanese_emoticon(filepath, emoticon)
   load_library(filepath)[:get_emoticon][emoticon] ||= "Sorry, that emoticon was not found"
 end
 
+<<<<<<< HEAD
 def get_english_meaning(filepath, emoticon)
   load_library(filepath)[:get_meaning][emoticon] ||= "Sorry, that emoticon was not found"
+=======
+def get_english_meaning(filepath, meaning)
+  load_library(filepath)[:get_meaining][meaning] ||= "Sorry, that emoticon was not found"
+>>>>>>> ce56cead2ce0db974967b8baa02839030a54ba41
 end
